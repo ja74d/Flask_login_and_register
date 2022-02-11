cbef6ed43ce53b157855aae1627a454644b8008e
javad pournosrat <70747310+ja74d@users.noreply.github.com>
2022-02-12 00:50:30 +0330

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ class Config(object):
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'
     # + os.path.join(os.path.dirname(__file__), 'app.db')
     #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://javad:40517780@localhost/blog_db'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://kriteofvhxekvf:dcdff79cffbfcac330d01a774899e310ab135bbb154d8444b20168107c7db252@ec2-34-230-110-100.compute-1.amazonaws.com:5432/dfrngetsr3n9lo'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or ''
